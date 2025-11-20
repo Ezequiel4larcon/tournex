67b3119b1812e24462c0a178b4a8d8bc257cf360
Ezequiel4larcon <alarconezequiel@yahoo.com.ar>
2025-11-20 14:38:25 -0300

    app funcionando

--- proyecto-tournex/frontend/src/router/AppRouter.jsx
+++ proyecto-tournex/frontend/src/router/AppRouter.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import { ForumProvider } from './context/ForumContext';
-import Navbar from './components/Navbar';
-import ProtectedRoute from './components/ProtectedRoute';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import NewPost from './pages/NewPost';
-import CommentDetail from './pages/CommentDetail';
+import { AuthProvider } from '../context/AuthContext';
+import { ForumProvider } from '../context/ForumContext';
+import Navbar from '../components/Navbar';
+import ProtectedRoute from '../components/ProtectedRoute';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import NewPost from '../pages/NewPost';
+import CommentDetail from '../pages/CommentDetail';
 
 const AppRouter = () => {
   return (
